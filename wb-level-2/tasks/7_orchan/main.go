8b8b2f5a8d3c61e85825d08f83fbf70b49701e58
Ковгар Алексей Викторович <kovgar.aleksey@wb.ru>
2021-09-17 14:58:16 +0300

    +orchan

---
+++ wb-level-2/tasks/7_orchan/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-OrChan(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		//sig(20*time.Millisecond),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("done after %v", time.Since(start))
+}
+
+func OrChan(channels ...<-chan interface{}) <-chan interface{} {
+	res := make(chan interface{})
+	isCanc := false
+	for _, v := range channels {
+		go func(ch <-chan interface{}) {
+			select {
+			case <-ch:
+				if !isCanc {
+					isCanc = true
+					close(res)
+				}
+				return
+			case <-res:
+				return
+			}
+		}(v)
+	}
+	return res
+}
